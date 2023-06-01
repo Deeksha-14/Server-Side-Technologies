cd5346cf1e0374ab5feecd2271766e53cf939ac3
DEEKSHA KUSHWAH <deekshakushwah0@gmail.com>
2023-06-01 07:52:54 +0530

    Update calc.js

--- node.js/calc.js
+++ node.js/calc.js
@@ -1,14 +1,14 @@
- exports.addition = function(n1 , n2){
+ exports.add = function(n1,n2){
     return n1+n2;
 }
-//public function
-exports.perm = function(n, r)
+//private function
+const perm = function(n, r)
 {
     var a = factorial(n)/(factorial(n)-factorial(r))
     return a;
 }
-//private function
-const fact = function(n)
+//pubic function
+exports.fact = function(n)
 {f=1;
     for(var i=1; i<=n; i++)
     {
