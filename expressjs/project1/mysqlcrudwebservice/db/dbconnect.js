7bad265a838285371a7e25050af3a79d9ba17a1d
DEEKSHA KUSHWAH <deekshakushwah0@gmail.com>
2023-06-05 08:07:04 +0530

    Create dbconnect.js

---
+++ expressjs/project1/mysqlcrudwebservice/db/dbconnect.js
@@ -0,0 +1,23 @@
+const mysql=require("mysql");
+
+const mysqlconnection = mysql.createConnection({
+    host:'127.0.0.1',
+    user:'root',
+    password:'root123',
+    database:'prac',
+    port:3306
+})
+
+mysqlconnection.connect((err)=>{
+    if(err){
+        console.log("connection failed"+JSON.stringify(err))
+    }else{
+        console.log("connection successful")
+    }
+})
+
+module.exports=mysqlconnection;
+
+
+
+
