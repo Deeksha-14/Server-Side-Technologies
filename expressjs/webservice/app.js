8893714402747072b124deca854c8c45ddb432f6
DEEKSHA KUSHWAH <deekshakushwah0@gmail.com>
2023-06-06 22:07:40 +0530

    Update app.js

--- expressjs/webservice/app.js
+++ expressjs/webservice/app.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const app = express();
 const bodyparser = require("body-parser");
-const router = require('./router/routers.js')
+const router = require('./router/routers.js');
 
 
 
-app.use(bodyparser.urlencoded({extended:false}))
+app.use(bodyparser.urlencoded({extended:false}));
 
 
-app.use(bodyparser.json())
+app.use(bodyparser.json());
 
 app.use("/",router);
 
 app.listen(3002,function(){
-    console.log("server running at port 3002")
+    console.log("server running at port 3002");
 })
 
 
